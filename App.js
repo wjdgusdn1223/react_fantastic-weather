202f03da0eb9031ab772f9b1d1b0994aeaf682ce
JungHyeonWoo <wjdgusdn1223@gmail.com>
2018-07-17 23:10:56 +0900

    expo test complete

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello! I'm a Native Developer Now!!!</Text>
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
       </View>
     );
   }
@@ -15,7 +18,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'flex-end',
+    flexWrap: 'nowrap'
   },
+  redView: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red'
+  },
+  yellowView: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'yellow'
+  }
 });
