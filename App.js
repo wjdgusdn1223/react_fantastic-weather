b5198ace410d47a5eee76c92933427de74b57e8f
JungHyeonWoo <wjdgusdn1223@gmail.com>
2018-07-18 20:35:25 +0900

    weather component test

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import Weather from "./Weather"
 
 export default class App extends React.Component {
+  state = {
+    isLoaded: true
+  }
   render() {
+    const { isLoaded } = this.state;
+    console.log(isLoaded);
     return (
       <View style={styles.container}>
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
+        {isLoaded ? <Weather /> : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the fantastic weather</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -17,20 +24,20 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'flex-end',
-    flexWrap: 'nowrap'
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    flexWrap: "wrap"
   },
-  redView: {
-    width: 50,
-    height: 50,
-    backgroundColor: 'red'
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end"
   },
-  yellowView: {
-    width: 50,
-    height: 50,
-    backgroundColor: 'yellow'
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100,
+    paddingLeft: 25
   }
 });
